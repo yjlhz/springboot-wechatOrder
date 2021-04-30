f97a3a83235eef65f060bee9c15336f9f478e20e
yjlhz <2386987985@qq.com>
2021-05-01 00:53:03 +0800

    日志配置测试

---
+++ src/main/java/com/yjlhz/sell/loggerTest.java
@@ -0,0 +1,26 @@
+package com.yjlhz.sell;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class loggerTest {
+
+    @Test
+    public void test1(){
+        String name = "name";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("info....");
+        log.info("name: {},password: {}",name,password);
+        log.error("error....");
+    }
+}
